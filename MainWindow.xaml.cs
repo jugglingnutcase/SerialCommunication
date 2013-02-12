0f8114c1cc34f12166ceae3753aabfe5fb021d83
James Sconfitto <james@exele.com>
2013-02-12 12:54:14 -0500

    Shove the final color into my serial port

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReactiveUI;
+using ReactiveUI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
@@ -13,32 +15,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ReactiveUI;
-using ReactiveUI.Xaml;
+using System.IO.Ports;
 
-namespace RxUI_QCon
-{
+namespace RxUI_QCon {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, IViewFor<MainWindowViewModel>
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window, IViewFor<MainWindowViewModel> {
+        private SerialPort _serialPort;
+
+        public MainWindow() {
             ViewModel = new MainWindowViewModel();
             InitializeComponent();
+
+            _serialPort = new SerialPort("COM3", 9600);
+            _serialPort.Open();
 
             this.Bind(ViewModel, x => x.Red);
             this.Bind(ViewModel, x => x.Green);
             this.Bind(ViewModel, x => x.Blue);
 
             this.OneWayBind(ViewModel, x => x.FinalColor, x => x.FinalColor.Background);
-            this.BindCommand(ViewModel, x => x.Ok);
 
-            this.WhenAny(x => x.ViewModel.Ok, x => x.Value).Merge()
-                .Subscribe(_ => MessageBox.Show("It worked!"));
+            // And output some stuff to the serial port
+            this.WhenAny(x => x.ViewModel.FinalColor, x => x.Value).Where(x => x != null).Throttle(TimeSpan.FromSeconds(0.3), RxApp.DeferredScheduler).Subscribe(x => {
+                if (_serialPort.IsOpen) {
+                    _serialPort.WriteLine(String.Format("33,{0},{1},{2}", x.Color.R, x.Color.G, x.Color.B));
+                }
+            });
 
-            this.OneWayBind(ViewModel, x => x.Images);
         }
 
         public MainWindowViewModel ViewModel {
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -12,12 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ReactiveUI;
 using ReactiveUI.Xaml;
-using Newtonsoft.Json;
 
 namespace RxUI_QCon
 {
-    public class MainWindowViewModel : ReactiveObject
-    {
+    public class MainWindowViewModel : ReactiveObject {
         int _Red;
         public int Red {
             get { return _Red; }
@@ -41,15 +39,7 @@ namespace RxUI_QCon
             get { return _FinalColor.Value; }
         }
 
-        ObservableAsPropertyHelper<List<BitmapImage>> _Images;
-        public List<BitmapImage> Images {
-            get { return _Images.Value; }
-        }
-
-        public ReactiveCommand Ok { get; protected set; }
-
-        public MainWindowViewModel()
-        {
+        public MainWindowViewModel() {
             var whenAnyColorChanges = this.WhenAny(x => x.Red, x => x.Green, x => x.Blue,
                     (r, g, b) => Tuple.Create(r.Value, g.Value, b.Value))
                 .Select(intsToColor);
@@ -57,67 +47,23 @@ namespace RxUI_QCon
             whenAnyColorChanges
                 .Where(x => x != null)
                 .Select(x => new SolidColorBrush(x.Value))
-                .ToProperty(this, x => x.FinalColor);
-
-            Ok = new ReactiveCommand(whenAnyColorChanges.Select(x => x != null));
-
-            this.WhenAny(x => x.FinalColor, x => x.Value)
                 .Throttle(TimeSpan.FromSeconds(0.7), RxApp.DeferredScheduler)
-                .Select(x => imagesForColor(x.Color))
-                .Switch()
-                .ObserveOn(RxApp.DeferredScheduler)
-                .Select(imageListToImages)
-                .ToProperty(this, x => x.Images);
+                .ToProperty(this, x => x.FinalColor);
         }
 
-        Color? intsToColor(Tuple<int, int, int> colorsAsInts)
-        {
+        Color? intsToColor(Tuple<int, int, int> colorsAsInts) {
             byte? r = inRange(colorsAsInts.Item1), g = inRange(colorsAsInts.Item2), b = inRange(colorsAsInts.Item3);
 
             if (r == null || g == null || b == null) return null;
             return Color.FromRgb(r.Value, g.Value, b.Value);
         }
 
-        static byte? inRange(int value)
-        {
+        static byte? inRange(int value) {
             if (value < 0 || value > 255) {
                 return null;
             }
 
-            return (byte) value;
-        }
-
-        IObservable<ImageList> imagesForColor(Color sourceColor)
-        {
-            var queryParams = new[] {
-                new { k = "method", v = "flickr_color_search" },
-                new { k = "limit", v = "73" },
-                new { k = "offset", v = "0" },
-                new { k = "colors[0]", v = String.Format("{0:x2}{1:x2}{2:x2}", sourceColor.R, sourceColor.G, sourceColor.B) },
-                new { k = "weights[0]", v = "1" },
-            };
-
-            var query = queryParams.Aggregate("",
-                (acc, x) => String.Format("{0}&{1}={2}", acc, HttpUtility.UrlEncode(x.k), HttpUtility.UrlEncode(x.v)));
-
-            query = query.Substring(1);
-
-            var wc = new HttpClient();
-            var url = "http://labs.tineye.com/rest/?" + query;
-            wc.BaseAddress = new Uri(url);
-
-            return wc.GetStringAsync("").ToObservable()
-                .Select(JsonConvert.DeserializeObject<ImageList>);
-        }
-
-        List<BitmapImage> imageListToImages(ImageList imageList)
-        {
-            return imageList.result
-                .Select(x => "http://img.tineye.com/flickr-images/?filepath=labs-flickr/" + x.filepath)
-                .Select(x => {
-                    var ret = new BitmapImage(new Uri(x));
-                    return ret;
-                }).ToList();
+            return (byte)value;
         }
     }
 }
